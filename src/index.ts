fa9407b54f376c82b181bb207991dac05140eff1
Kory <kory.porter@hotmail.com>
2019-06-02 17:09:22 +1000

    removed comment

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,6 @@ import { RootServer } from "./servers/root";
 import { GopherNrlServer } from "./servers/nrl";
 import { GopherFileServer } from "./servers/file";
 
-/**
- * What this server should do:
- * - Given a source directory location, list the files/folders in gopher protocol.
- * - The client should be able to select a folder, which should then display the sub contents
- * - The client should be able to select a file, which should then sent back the contents of the file.
- * - We should support only .txt files
- * - Listen on port 70.
- */
-
 if (!process.env.PORT) {
   throw Error("PORT is required, please set it in the env file.");
 }
