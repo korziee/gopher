4aa3a43261189da16bba7b559ce1ec1b4b176766
Kory <kory.porter@hotmail.com>
2019-06-09 16:21:41 +1000

    added env vars for plugins

--- src/index.ts
+++ src/index.ts
@@ -10,27 +10,38 @@ if (!process.env.HOST) {
   throw Error("HOST is required, please set it in the env file.");
 }
 
+const plugins = [];
+
+if (process.env.GOPHER_NRL_PLUGIN === "true") {
+  plugins.push({
+    handler: "nrl",
+    descriptionShort: "NRL GAMES",
+    descriptionLong: "Check the scores of the current round of the NRL",
+    class: GopherNrlServer
+  });
+}
+if (process.env.GOPHER_FILE_PLUGIN === "true") {
+  if (!process.env.GOPHER_FILE_PLUGIN_DIRECTORY) {
+    throw new Error(
+      "File plugin directory is required if file plugin is set to true!"
+    );
+  }
+  plugins.push({
+    handler: "file",
+    descriptionShort: "FILE SERVER",
+    descriptionLong: "Some files!",
+    initParams: {
+      directory: process.env.GOPHER_FILE_PLUGIN_DIRECTORY,
+      debug: false
+    },
+    class: GopherFileServer
+  });
+}
+
 const server = new RootServer(
   process.env.HOST,
   parseInt(process.env.PORT, 10),
-  [
-    {
-      handler: "nrl",
-      descriptionShort: "NRL GAMES",
-      descriptionLong: "Check the scores of the current round of the NRL",
-      class: GopherNrlServer
-    },
-    {
-      handler: "file",
-      descriptionShort: "FILE SERVER",
-      descriptionLong: "Some files!",
-      initParams: {
-        directory: "/Users/koryporter/Projects/gopher/directory",
-        debug: false
-      },
-      class: GopherFileServer
-    }
-  ]
+  plugins
 );
 
 server.init().then(() => {
